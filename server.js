0ee57b96f1284bf063d2c17536caa050cd4a6911
aniket-code-cf <aniketkumar74974@gmail.com>
2024-07-02 01:06:01 +0530

    change

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-app.use(express.static(path.join(__dirname,'./frontend/build')));
+app.use(express.static(path.join(__dirname,'./frontend')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -36,7 +36,7 @@ app.use("/api/v1/product", productRoutes);
 //rest api
 app.use('*',function(req,res)
 {
-  res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
+  res.sendFile(path.join(__dirname,'./frontend/index.html'));
 }
 );
 
